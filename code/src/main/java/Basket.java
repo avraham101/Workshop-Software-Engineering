cb076e4297a537a81e42c08e768e119a6cba9a79
yuvalhuck1 <sabay@post.bgu.ac.il>
2020-03-29 22:15:19 +0300

    test

--- code/src/main/java/Basket.java
+++ code/src/main/java/Basket.java
@@ -3,4 +3,20 @@ import java.util.List;
 public class Basket {
     private Store store;
     private List<Product> products;
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
