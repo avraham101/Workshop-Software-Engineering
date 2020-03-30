7009e9d4a1ef3eb0eb1ebb643678cb82fe85a85b
yuvalhuck1 <sabay@post.bgu.ac.il>
2020-03-30 14:16:50 +0300

    junit

--- code/src/main/java/Permission.java
+++ code/src/main/java/Permission.java
@@ -4,7 +4,36 @@ public class Permission {
 
     private Subscribe owner;
     private Store store;
-    HashSet<PermissionType> permissionType;
+    private HashSet<PermissionType> permissionType;
 
+    public Permission(Subscribe owner, Store store) {
+        this.owner = owner;
+        this.store = store;
+        permissionType=new HashSet<>();
+    }
+
+    public Subscribe getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Subscribe owner) {
+        this.owner = owner;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public HashSet<PermissionType> getPermissionType() {
+        return permissionType;
+    }
+
+    public void setPermissionType(HashSet<PermissionType> permissionType) {
+        this.permissionType = permissionType;
+    }
 }
 
