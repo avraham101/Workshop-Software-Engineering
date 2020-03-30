c7e7024c71abc8fd2edb9848f24b1648aad66b74
Niv Shirazi <nivshir@post.bgu.ac.il>
2020-03-30 14:38:38 +0300

    add tests

--- code/src/main/java/Category.java
+++ code/src/main/java/Category.java
@@ -1,7 +1,30 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Category {
 
     private String name; //unique name
     private List<Product> products;
+
+    public Category(String name,Product product) {
+        this.name = name;
+        products=new ArrayList<>();
+        products.add(product);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
