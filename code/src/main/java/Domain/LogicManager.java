5077c01ac69698f252b4ad2d5c64b70cac832fc9
Niv Shirazi <nivshir@post.bgu.ac.il>
2020-04-01 19:48:19 +0300

    fix logger

--- code/src/main/java/Domain/LogicManager.java
+++ code/src/main/java/Domain/LogicManager.java
@@ -14,6 +14,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.List;
+import java.util.logging.Level;
 
 public class LogicManager {
     private HashMap<String, Subscribe> users;
@@ -75,7 +76,8 @@ public class LogicManager {
      * @return true if the register complete, otherwise false
      */
     public boolean register(String userName, String password) {
-        //TODO add to logger
+        loggerSystem.getEventLogger().logp(Level.INFO, "Logic Manager", "register", "register: {0} {1}",
+                new Object[] { userName, password });
         if(!validName(userName) || !validPassword(password)) {
             return false;
         }
