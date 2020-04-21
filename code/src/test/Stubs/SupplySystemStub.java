734fdec46255ddd92eb4dc385734ef47fbf65a4d
Niv Shirazi <nivshir@post.bgu.ac.il>
2020-04-21 11:36:45 +0300

    added test with stub external systems #84 #31
    added external systems stub

---
+++ code/src/test/Stubs/SupplySystemStub.java
@@ -0,0 +1,22 @@
+package Stubs;
+
+import DataAPI.DeliveryData;
+import Systems.SupplySystem.SupplySystem;
+
+public class SupplySystemStub extends SupplySystem {
+
+    @Override
+    public boolean connect() {
+        return false;
+    }
+
+    @Override
+    public boolean deliver(DeliveryData deliveryData) {
+        return false;
+    }
+
+    @Override
+    public boolean isConnected() {
+        return false;
+    }
+}
