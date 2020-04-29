3ea935e49811189cb83b040dd57fc67a02ccb38b
tal773 <tal9@post.bgu.ac.il>
2020-04-29 23:19:31 +0300

    get admin

--- code/src/main/java/Server/Controllers/AdminController.java
+++ code/src/main/java/Server/Controllers/AdminController.java
@@ -3,12 +3,18 @@ package Server.Controllers;
 import DataAPI.UserData;
 import Service.SingleService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AdminController {
+
+    @GetMapping("admin")
+    public String getAdminPage(){
+        return "admin";
+    }
 
     @PostMapping("admin")
     @ResponseBody
