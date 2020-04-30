adbbb9e064f75abccf7a34560919001f501077ae
Niv Shirazi <nivshir@post.bgu.ac.il>
2020-04-30 13:27:25 +0300

    add product to cart
    use case 2.7.4 gui #183

--- code/src/main/front_end/src/App.js
+++ code/src/main/front_end/src/App.js
@@ -3,13 +3,15 @@ import { Router, Route, Switch, withRouter } from 'react-router-dom';
 import GuestIndex from './Screen/Guest/GuestIndex'
 import Register from './Screen/Guest/Register'
 import history from './Screen/history'
+import AddProductToCart from './Screen/Guest/AddProductToCart';
 
 class App extends Component {
 
   render() {
     return (<Router history={history}>
             <Switch>
-              <Route path="/register" component={withRouter(Register)} />          
+              <Route path="/register" component={withRouter(Register)} />    
+              <Route path="/addToCart" component={withRouter(AddProductToCart)} />      
               <Route path="/" component={withRouter(GuestIndex)} exact />
             </Switch>
           </Router>)
