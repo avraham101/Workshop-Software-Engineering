d7bc2fd97d23c54af560c7e57b34f647002a51b8
tal773 <tal9@post.bgu.ac.il>
2020-05-01 13:16:13 +0300

    websocket configure

---
+++ code/src/main/java/Server/RealTime/WebSocketController.java
@@ -0,0 +1,47 @@
+package Server.RealTime;
+
+import DataAPI.OpCode;
+import DataAPI.Response;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+import java.util.Map;
+
+@Controller
+public class WebSocketController {
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/message")
+    @SendToUser("/queue/reply")
+    public ResponseEntity<?> processMessageFromClient(@Payload String message, Principal principal) throws Exception {
+        String name = new Gson().fromJson(message, Map.class).get("name").toString();
+        //messagingTemplate.convertAndSendToUser(principal.getName(), "/queue/reply", name);
+        System.out.println(principal.getName());
+         Response<String> response = new Response<>("nice", OpCode.Success);
+        return getResponseEntity(response);
+    }
+
+    @MessageExceptionHandler
+    @SendToUser("/queue/errors")
+    public String handleException(Throwable exception) {
+        return exception.getMessage();
+    }
+    private ResponseEntity<?> getResponseEntity(Response<?> response) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        return new ResponseEntity<>(response, headers, HttpStatus.CREATED);
+    }
+
+}
