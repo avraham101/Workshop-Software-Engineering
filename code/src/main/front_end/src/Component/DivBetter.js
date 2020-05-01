17ed16ea2fa9e93bc6c068c4f35faad3f3ae7976
avraham calev <caleva@post.bgu.ac.il>
2020-05-01 04:11:20 +0300

    almost done with discount manager

---
+++ code/src/main/front_end/src/Component/DivBetter.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class DivBetter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hoverd: false,
+    };
+  }
+
+  getStyle() {
+    return this.state.hoverd
+      ? { backgroundColor: "#92BAFF",border:'1px solid black',textAlign:"center"}
+      : { backgroundColor: "",border:'1px solid black',textAlign:"center"};
+  }
+
+  render() {
+    return (
+      <div
+        style={this.getStyle()}
+        onMouseEnter={() => this.setState({ hoverd: true })}
+        onMouseLeave={() => this.setState({ hoverd: false })}
+        onClick={this.props.onClick}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default DivBetter;
+
+const style_sheet = {
+  backgroundColor: "#F9F9F9",
+  padding: 1,
+  width: "100%",
+};
