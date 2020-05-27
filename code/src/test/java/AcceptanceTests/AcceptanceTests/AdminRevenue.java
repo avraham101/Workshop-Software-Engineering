074b3844c37ee8c93bac20597240768d48c9089d
Roy Levy <roy4@post.bgu.ac.il>
2020-05-27 17:37:11 +0300

    start Acceptance Tests - AdminRevenue

--- code/src/test/java/AcceptanceTests/AcceptanceTests/AdminRevenue.java
+++ code/src/test/java/AcceptanceTests/AcceptanceTests/AdminRevenue.java
@@ -25,12 +25,13 @@ public class AdminRevenue extends AcceptanceTests{
     }
 
     public void setUpWithDicount(){
+        userToCheck = users.get(1);
         addUserStoresAndProducts(admin);
         DiscountTestData discountTestData=new DiscountTestData(50,"appleTest");
-        bridge.addDiscount(admin.getId(), discountTestData, "store0Test");
-        bridge.addToUserCart(admin.getId(),products.get(0),1);
-        bridge.buyCart(admin.getId(),validPayment,validDelivery);
-        bridge.login(admin.getId(),admin.getUsername(),admin.getPassword());
+        boolean added = bridge.addDiscount(admin.getId(), discountTestData, "store0Test");
+        added = bridge.addToUserCart(admin.getId(),products.get(0),1);
+        added = bridge.buyCart(admin.getId(),validPayment,validDelivery);
+        added = bridge.login(admin.getId(),admin.getUsername(),admin.getPassword());
     }
 
 
