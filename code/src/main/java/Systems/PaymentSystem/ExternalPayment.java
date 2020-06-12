6587933bb9db4a7d6bef193090f1ec9d77eb5ecb
avraham calev <caleva@post.bgu.ac.il>
2020-06-12 03:26:57 +0300

    Added support to external systems, however need to change our implementation. TODO: add more field to our implementation, TODO: add External System Delivry Test, TODO: check if need to use external system delivery cancle

--- code/src/main/java/Systems/PaymentSystem/ExternalPayment.java
+++ code/src/main/java/Systems/PaymentSystem/ExternalPayment.java
@@ -1,21 +1,63 @@
 package Systems.PaymentSystem;
 
 import DataAPI.PaymentData;
+import Systems.HttpConnection;
 
 public class ExternalPayment extends PaymentSystem{
 
     @Override
     public boolean connect() {
+        String url = "https://cs-bgu-wsep.herokuapp.com/";
+        String method = "POST";
+        String params = "action_type=handshake";
+        String msg = "";
+        HttpConnection httpConnection = new HttpConnection();
+        String result = httpConnection.send(url,method,params,msg);
+        if(result!=null && result.equals("OK"))
+            return true;
         return false;
     }
 
     @Override
     public boolean pay(PaymentData paymentData) {
+        String url = "https://cs-bgu-wsep.herokuapp.com/";
+        String method = "POST";
+        String params = "action_type=pay";
+        params += "&card_number="+paymentData.getCreditCard();
+        params += "&month=";
+        params += "&year=";
+        params += "&holder=";
+        params += "&ccv=";
+        params += "&id=";
+        String msg = "";
+        HttpConnection httpConnection = new HttpConnection();
+        String result = httpConnection.send(url,method,params,msg);
+        System.out.println(result);
+        if(result!=null) {
+            int tmp = Integer.valueOf(result);
+            if(tmp >= 10000 && tmp <= 100000) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean cancel(PaymentData paymentData) {
+        String url = "https://cs-bgu-wsep.herokuapp.com/";
+        String method = "POST";
+        String params = "action_type=cancel_pay";
+        params += "&transaction_id=";
+        String msg = "";
+        HttpConnection httpConnection = new HttpConnection();
+        String result = httpConnection.send(url,method,params,msg);
+        System.out.println(result);
+        if(result!=null) {
+            int tmp = Integer.valueOf(result);
+            if(tmp == 1) {
+                return true;
+            }
+        }
         return false;
     }
 
--- code/src/main/java/Systems/SupplySystem/ExternalSupply.java
+++ code/src/main/java/Systems/SupplySystem/ExternalSupply.java
@@ -1,17 +1,43 @@
 package Systems.SupplySystem;
 
 import DataAPI.DeliveryData;
+import Systems.HttpConnection;
 
 public class ExternalSupply extends SupplySystem {
 
     @Override
     public boolean connect() {
+        String url = "https://cs-bgu-wsep.herokuapp.com/";
+        String method = "POST";
+        String params = "action_type=handshake";
+        String msg = "";
+        HttpConnection httpConnection = new HttpConnection();
+        String result = httpConnection.send(url,method,params,msg);
+        if(result!=null && result.equals("OK"))
+            return true;
         return false;
     }
 
     @Override
     public boolean deliver(DeliveryData deliveryData) {
+        String url = "https://cs-bgu-wsep.herokuapp.com/";
+        String method = "POST";
+        String params = "action_type=supply";
+        params += "&name=";
+        params += "&address="+deliveryData.getAddress();
+        params += "&city=";
+        params += "&country="+deliveryData.getCountry();
+        params += "&zip=";
+        String msg = "";
+        HttpConnection httpConnection = new HttpConnection();
+        String result = httpConnection.send(url,method,params,msg);
+        System.out.println(result);
+        if(result!=null) {
+            int tmp = Integer.valueOf(result);
+            if(tmp >= 10000 && tmp <= 100000) {
+                return true;
+            }
+        }
         return false;
     }
-    
 }
