3b6a3611aa5f761b1767a113d750c0673506e7c0
avraham calev <caleva@post.bgu.ac.il>
2020-06-19 16:53:38 +0300

    added the image

---
+++ code/src/main/resources/templates/precache-manifest.2b48c6a4297540df824c0563961df424.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ead899c02b8fea6b78948d563f156d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be1b5526962439867b27",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "48ae722be6b41c393ebf",
+    "url": "/static/js/2.6bea26a7.chunk.js"
+  },
+  {
+    "revision": "081a31839f076f958138854bef4f211d",
+    "url": "/static/js/2.6bea26a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1b5526962439867b27",
+    "url": "/static/js/main.5a102dea.chunk.js"
+  },
+  {
+    "revision": "6aadd2d4dd009af51c8d",
+    "url": "/static/js/runtime-main.31e78cdb.js"
+  },
+  {
+    "revision": "31ca325933882f1c5a07ac452cddcf3a",
+    "url": "/static/media/404_logo.31ca3259.jpg"
+  },
+  {
+    "revision": "effb3e87a17059b55f3c004165127ca7",
+    "url": "/static/media/store.effb3e87.jpg"
+  }
+]);
